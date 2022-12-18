616024c26027c1db61a3a2b1f3436f87f3b27132
Lib3Rt9 <phamgiaphuc011709@gmail.com>
2022-12-19 02:19:15 +0700

    increase 15 to 20 most frequent numbers

--- 10_most_freq_nums.py
+++ 10_most_freq_nums.py
@@ -102,16 +102,16 @@ def check_frequent_numbers():
 
     # int_counter = Counter(all_num_list)             # all_num_list if the list of number in form of INTEGER
 
-    all_num_str_count = all_num_str_count.most_common(15)       # counter for list of numbers but in form of string
-    jackpot_1_str_count = jackpot_1_str_count.most_common(15)       # counter for list of numbers but in form of string
-    jackpot_2_str_count = jackpot_2_str_count.most_common(15)       # counter for list of numbers but in form of string
+    all_num_str_count = all_num_str_count.most_common(20)       # counter for list of numbers but in form of string
+    jackpot_1_str_count = jackpot_1_str_count.most_common(20)       # counter for list of numbers but in form of string
+    jackpot_2_str_count = jackpot_2_str_count.most_common(20)       # counter for list of numbers but in form of string
 
     # int_counter = int_counter.most_common(10)       # counter for list of integer numbers
 
     print()
-    print("10 most frequent of ALL NUMBERS (string): \n", all_num_str_count, "\n")
-    print("10 most frequent of JACKPOT (string): \n", jackpot_1_str_count, "\n")
-    print("10 most frequent of JACKPOT 2 (string): \n", jackpot_2_str_count, "\n")
+    print("20 most frequent of ALL NUMBERS (string): \n", all_num_str_count, "\n")
+    print("20 most frequent of JACKPOT (string): \n", jackpot_1_str_count, "\n")
+    print("20 most frequent of JACKPOT 2 (string): \n", jackpot_2_str_count, "\n")
 
     # print("10 most frequent of ALL NUMBERS (integer): ", int_counter, "\n")
 
